f2c16b656b5bcd6e09ff06f729c3ad616c998d94
Atour <28668597+amousavigourabi@users.noreply.github.com>
2023-07-19 13:37:55 +0200

    Add field modification functionality (#7)
    To set non-static fields, both final and non-final. For all access
    levels.

--- src/main/java/me/atour/reflex/Creator.java
+++ src/main/java/me/atour/reflex/Creator.java
@@ -1,5 +1,6 @@
 package me.atour.reflex;
 
+import java.lang.reflect.Field;
 import lombok.NonNull;
 
 /**
@@ -22,5 +23,34 @@ public class Creator {
    */
   @NonNull public <T> T instantiate(@NonNull Class<T> clazz) throws InstantiationException {
     return unsafe.allocateInstance(clazz);
+  }
+
+  /**
+   * Sets a field in the given instance.
+   *
+   * @param instance the instance to modify
+   * @param field the {@link Field} to modify
+   * @param value the value to set the field to
+   */
+  public void setField(@NonNull Object instance, @NonNull Field field, Object value) {
+    long offset = unsafe.objectFieldOffset(field);
+    if (field.getType().isPrimitive()) {
+      unsafe.putPrimitive(instance, offset, value, field.getType());
+    } else {
+      unsafe.putObject(instance, offset, value);
+    }
+  }
+
+  /**
+   * Sets a field in the given instance.
+   *
+   * @param instance the instance to modify
+   * @param field the field to modify, represented as a {@link String}
+   * @param value the value to set the field to
+   * @param <T> type parameter for the instance type
+   * @throws NoSuchFieldException when the given field representation does not exist
+   */
+  public <T> void setField(@NonNull T instance, @NonNull String field, Object value) throws NoSuchFieldException {
+    setField(instance, instance.getClass().getDeclaredField(field), value);
   }
 }
--- src/test/java/me/atour/reflex/CreatorTest.java
+++ src/test/java/me/atour/reflex/CreatorTest.java
@@ -2,28 +2,73 @@ package me.atour.reflex;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Field;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CreatorTest {
 
+  @Getter
+  @RequiredArgsConstructor
+  private static class SetClass {
+    private final Integer integer;
+    private final double doubleValue;
+    private final char character;
+  }
+
+  private static Creator creator;
+
+  @BeforeAll
+  public static void beforeAll() {
+    creator = new Creator();
+  }
+
   @Test
   void instantiateInteger() throws InstantiationException {
-    Creator creator = new Creator();
     int i = creator.instantiate(Integer.class);
     assertThat(i).isEqualTo(0);
   }
 
   @Test
   void instantiateDouble() throws InstantiationException {
-    Creator creator = new Creator();
-    double i = creator.instantiate(Double.class);
-    assertThat(i).isEqualTo(0.0);
+    double d = creator.instantiate(Double.class);
+    assertThat(d).isEqualTo(0.0);
   }
 
   @Test
-  void instantiateChar() throws InstantiationException {
-    Creator creator = new Creator();
-    char i = creator.instantiate(Character.class);
-    assertThat(i).isEqualTo('\u0000');
+  void instantiateCharacter() throws InstantiationException {
+    char c = creator.instantiate(Character.class);
+    assertThat(c).isEqualTo('\u0000');
+  }
+
+  @Test
+  void setInteger() throws NoSuchFieldException {
+    SetClass dummy = new SetClass(37, 20.0, 'z');
+    creator.setField(dummy, "integer", -1);
+    creator.setField(dummy, "integer", 1);
+    assertThat(dummy.getInteger()).isEqualTo(1);
+    assertThat(dummy.getDoubleValue()).isEqualTo(20.0);
+    assertThat(dummy.getCharacter()).isEqualTo('z');
+  }
+
+  @Test
+  void setDouble() throws NoSuchFieldException {
+    SetClass dummy = new SetClass(-91, 0.01, '_');
+    Field field = SetClass.class.getDeclaredField("doubleValue");
+    creator.setField(dummy, field, -1.0);
+    assertThat(dummy.getInteger()).isEqualTo(-91);
+    assertThat(dummy.getDoubleValue()).isEqualTo(-1.0);
+    assertThat(dummy.getCharacter()).isEqualTo('_');
+  }
+
+  @Test
+  void setCharacter() throws NoSuchFieldException {
+    SetClass dummy = new SetClass(1, 2.0, 'a');
+    creator.setField(dummy, "character", 'b');
+    assertThat(dummy.getInteger()).isEqualTo(1);
+    assertThat(dummy.getDoubleValue()).isEqualTo(2.0);
+    assertThat(dummy.getCharacter()).isEqualTo('b');
   }
 }
